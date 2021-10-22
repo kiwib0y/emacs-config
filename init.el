e27baec77309bc5b2ea5085526634b541a645277
KiwiB0y <caki6ko@yahoo.com>
2021-10-22 13:45:15 +0200

    Remove old rust-mode code

--- init.el
+++ init.el
@@ -252,20 +252,6 @@
   :config
   (add-hook 'rust-mode-hook 'cargo-minor-mode))
 
-
-;; rust programming setup
-(use-package rust-mode
-  :config
-  (add-hook 'racer-mode-hook #'eldoc-mode)
-  (add-hook 'racer-mode-hook #'company-mode)
-  (define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
-  (setq rust-format-on-save t)
-  (setq rust-indent-offset 4))
-
-(use-package cargo-mode
-  :config
-  (add-hook 'rust-mode-hook 'cargo-minor-mode))
-
 ;; golang mode setup
 (use-package go-mode
   :config
