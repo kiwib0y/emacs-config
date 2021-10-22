bec85ec196ae9639645d3cf4cbe75fe813637a15
KiwiB0y <caki6ko@yahoo.com>
2021-10-22 13:37:09 +0200

    Add rust-mode with rust-analyzer support

--- init.el
+++ init.el
@@ -239,6 +239,22 @@
 
 ;; rust programming setup
 (use-package rust-mode
+  :hook (rust-mode . lsp-mode)
+  :config
+  (add-hook 'lsp-rust-server #'rust-analyzer)
+  (add-hook 'racer-mode-hook #'eldoc-mode)
+  (add-hook 'racer-mode-hook #'company-mode)
+  (define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
+  (setq rust-format-on-save t)
+  (setq rust-indent-offset 4))
+
+(use-package cargo-mode
+  :config
+  (add-hook 'rust-mode-hook 'cargo-minor-mode))
+
+
+;; rust programming setup
+(use-package rust-mode
   :config
   (add-hook 'racer-mode-hook #'eldoc-mode)
   (add-hook 'racer-mode-hook #'company-mode)
