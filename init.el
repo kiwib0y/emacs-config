02fcc59c8333d0dac82074d44871b5285f6a0387
KiwiB0y <caki6ko@yahoo.com>
2021-11-25 21:58:22 +0100

    Add lsp in clojure-mode as a hook

--- init.el
+++ init.el
@@ -318,6 +318,7 @@
   :ensure t
   :config
   (add-hook 'clojure-mode-hook #'paredit-mode)
+  (add-hook 'clojure-mode-hook #'lsp)
   (add-hook 'clojure-mode-hook #'company-mode)
   (add-hook 'clojure-mode-hook #'subword-mode)
   (add-hook 'clojure-mode-hook #'rainbow-delimiters-mode))
