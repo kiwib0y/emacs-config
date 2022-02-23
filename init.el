78af9761afaa1b91afc7c52e3e42dfdc08812351
KiwiB0y <caki6ko@yahoo.com>
2022-02-23 19:23:16 +0100

    Update init.el reorder package archives

--- init.el
+++ init.el
@@ -103,9 +103,9 @@
 ;; initialize package source
 (require 'package)
 (setq package-archives '(("melpa-stable" . "https://stable.melpa.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")
 			 ("elpa" . "https://elpa.gnu.org/packages/")
-			 ("org" . "https://orgmode.org/elpa/")
-                         ("melpa" . "https://melpa.org/packages/")))
+			 ("org" . "https://orgmode.org/elpa/")))
 
 ;; check package sources
 (package-initialize)
